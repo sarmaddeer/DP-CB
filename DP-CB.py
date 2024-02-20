181ed122d0db72391e2f329bf1ade30cbd1bd320
sarmaddeer <145012327+sarmaddeer@users.noreply.github.com>
2024-02-20 05:59:23 +0500

    Update DP-CB.py

--- DP-CB.py
+++ DP-CB.py
@@ -266,7 +266,7 @@ def get_sql_keywords(input):
 def find_match(input):
     try:
         xq = openai.embeddings.create(input=input, model=model).data[0].embedding
-        result = index.query([xq], top_k=5, include_metadata=True)
+        result = index.query(vector= [xq], top_k=5, include_metadata=True)
         print(result)
         return result['matches'][0]['metadata']['text']+result['matches'][1]['metadata']['text']
     
